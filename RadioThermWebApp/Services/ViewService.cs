8dfb56e0034f82389f5303a2670bf7f20af1bfce
Cainan Whelchel <c.whelchel@gmail.com>
2022-08-01 09:34:52 -0400

    Add ViewService support to webapp

--- RadioThermWebApp/Services/ViewService.cs
+++ RadioThermWebApp/Services/ViewService.cs
@@ -1,18 +1,28 @@
 ﻿using RadioThermLib.Services;
 using RadioThermLib.ViewModels;
+using RadioThermWebApp.Pages;
+using RadioThermWebApp.Shared;
 
 namespace RadioThermWebApp.Services
 {
     public class ViewService : IViewService
     {
+        public EventHandler<ShowDialogsEventArgs> OnShowDialog;
+
         public void ShowDialog(string title, string message)
         {
-            throw new NotImplementedException();
+            OnShowDialog?.Invoke(this, new ShowDialogsEventArgs { Title=title, Message=message});
         }
 
         public void ShowThermostatDetails(ThermostatViewModel viewModel)
         {
             throw new NotImplementedException();
         }
+    }
+
+    public class ShowDialogsEventArgs : EventArgs
+    {
+        public string? Title { get; set; }
+        public string? Message { get; set; }
     }
 }
