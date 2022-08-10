514566010363327f4a50146b8ca461719986f6a7
Cainan Whelchel <c.whelchel@gmail.com>
2022-08-09 22:15:05 -0400

    Change bindings in new Program views to new Program ViewModels.

--- RadioThermLib/ViewModels/ProgramViewModel.cs
+++ RadioThermLib/ViewModels/ProgramViewModel.cs
@@ -1,37 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using RadioThermLib.Models;
-using RadioThermLib.Services;
 
 namespace RadioThermLib.ViewModels
 {
     public class ProgramViewModel : ObservableObject
     {
-        private readonly ThermostatProgram program;
-
-        private ObservableCollection<DayProgram?> dayPrograms;
+        private ObservableCollection<DayProgramViewModel?> dayPrograms;
 
         public ProgramViewModel(ThermostatProgram program)
         {
-            this.program = program;
-
-            this.DayPrograms = new ObservableCollection<DayProgram?>();
+            this.dayPrograms = new ObservableCollection<DayProgramViewModel?>();
 
             for (int i = 0; i < 7; i++)
             {
-                DayPrograms.Add(this.program.GetDayProgram(i));
+                DayPrograms.Add(new DayProgramViewModel(program.GetDayProgram(i)!));
             }
         }
 
-        public ObservableCollection<DayProgram?> DayPrograms
+        public ObservableCollection<DayProgramViewModel?> DayPrograms
         {
             get => dayPrograms;
             set => SetProperty(ref dayPrograms, value);
+        }
+    }
+
+    public class DayProgramViewModel : ObservableObject
+    {
+        private string day;
+        private ObservableCollection<TimeProgramViewModel> data;
+
+        public DayProgramViewModel(DayProgram dayProgram)
+        {
+            this.day = dayProgram.Day.ToString();
+            this.data = new ObservableCollection<TimeProgramViewModel>(
+                dayProgram.ProgramData.ConvertAll(input => new TimeProgramViewModel(input)));
+        }
+
+        public string DayOfWeek
+        {
+            get => day;
+            set => SetProperty(ref day, value);
+        }
+
+        public ObservableCollection<TimeProgramViewModel> Data
+        {
+            get => data;
+            set => SetProperty(ref data, value);
+        }
+    }
+
+    public class TimeProgramViewModel : ObservableObject
+    {
+        private float programmedTemp;
+        private TimeSpan startTime;
+
+        public TimeProgramViewModel(TimeProgram data)
+        {
+            this.programmedTemp = data.ProgrammedTemp;
+            this.startTime = data.Time;
+        }
+
+        public float ProgrammedTemp
+        {
+            get => programmedTemp;
+            set => SetProperty(ref programmedTemp, value);
+        }
+
+        public TimeSpan StartTime
+        {
+            get => startTime;
+            set => SetProperty(ref startTime, value);
         }
     }
 }
